78c7f7492f7dc0d1c80809caf5c8e6efa2064bca
Will <Will@willspics.co.uk>
2010-11-05 22:59:02 +0000

    Set up structure for serving css/images, added index page (not tested with data) and broke up conepts in to mako templates

--- DontCheckMe.In/dontcheckmein/model/objects.py
+++ DontCheckMe.In/dontcheckmein/model/objects.py
@@ -7,10 +7,12 @@ class IgnoreFile(Base):
     __tablename__ = "ignorefile"
 
     id = Column(Integer, primary_key=True)
-    submitted_by = Column(Unicode(100))
+    submitted_by = Column(Unicode(50))
     submitted_date = Column(DateTime)
+    title = Column(Unicode(130))
+    desc = Column(Unicode)
     content = Column(Unicode)
     
     def __str__(self):
-      return content
+      return title
 	
